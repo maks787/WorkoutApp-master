473f54aea51710ff9a04e490fce74939ec6f5af7
Opilane TTHK <opilane@tthk.ee>
2024-05-29 10:47:52 +0300

    aaa
    aaa

---
+++ ABSWork.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls;
+using WorkoutApp.Models;
+using System.Collections.ObjectModel;
+
+namespace WorkoutApp
+{
+    public partial class ABSWork : ContentPage
+    {
+        public ObservableCollection<WorkoutDay> Days { get; set; }
+
+        public ABSWork()
+        {
+            InitializeComponent();
+
+            // Создание списка дней с описанием тренировок
+            Days = new ObservableCollection<WorkoutDay>();
+            for (int i = 1; i <= 30; i++)
+            {
+                Days.Add(new WorkoutDay
+                {
+                    Day = $"Day {i}",
+                    Description = $"Full Body Workout details for day {i}."
+                });
+            }
+
+            // Установка контекста данных для привязки
+            BindingContext = this;
+        }
+    }
+}
